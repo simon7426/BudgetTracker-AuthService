885ab8d1365b6d0af2595d2d74e42ce1f25159bc
Simon Islam <simon.islam@brilliant.com.bd>
2021-09-13 21:28:34 +0600

    init

---
+++ core/views.py
@@ -0,0 +1,11 @@
+from rest_framework import response
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class Alive(APIView):
+    def get(self, request):
+        response_obj = {
+            'status': 'success',
+            'message': 'Alive'
+        }
+        return Response(response_obj)
